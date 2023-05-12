49aabd223552303803479ec1f11edbcabc7ee3cd
Maxime131 <nmaxime131@gmail.com>
2023-05-12 17:23:57 +0200

    initial commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        $data["title"] = "Home";
+	
+		$data["content"] = view("frontend/home", $data);
+        return view('index', $data);
+    }
+
+    public function about()
+    {
+        $data["title"] = "Ab";
+	
+		$data["content"] = view("frontend/about", $data);
+        return view('page', $data);
+    }
+
+    public function services(){
+        $data["title"] = "Services";
+	
+		$data["content"] = view("frontend/service", $data);
+        return view('page', $data);
+    }
+
+    public function Product(){
+        $data["title"] = "product";
+	
+		$data["content"] = view("frontend/product", $data);
+        return view('page', $data);
+    }
+    public function Bees(){
+        $data["title"] = "bees";
+	
+		$data["content"] = view("frontend/bees", $data);
+        return view('page', $data);
+    }
+    public function Contact(){
+        $data["title"] = "contact";
+	
+		$data["content"] = view("frontend/contact", $data);
+        return view('page', $data);
+    }
+}
